ccdec289524b22d3ee3f79176cfb9d77dbde0c89
Niayesh Kakekhani <Kakeniayesh@gmail.com>
2024-05-08 19:07:04 +0330

    Add first box

--- src/Components/UserComments.jsx
+++ src/Components/UserComments.jsx
@@ -14,35 +14,84 @@ const UserComments = () => {
   }, []);
 
   return (
-    <div className="w-full flex justify-center  dark:bg-primary_Color_Light">
-      <div className="flex flex-col gap-4 p-3 w-96 sm:w-[500px] md:w-[1000px] lg:w-[1280px]  xl:w-[1596px] bg-gray-500">
-        <div className="flex flex-col gap-2">
-          <h1 className="font-semibold text-2xl">آنچه مشتریان ما می گویند</h1>
-          <div className="flex justify-between ">
-            <p>
+    <div className="w-full flex justify-center  dark:bg-primary_dark">
+      <div className="flex flex-col gap-4 p-1 w-96 sm:w-[500px] md:w-[1000px] lg:w-[1280px]  xl:w-[1596px]">
+        <div className="flex flex-col gap-4">
+          <h1 className="font-semibold text-5xl dark:text-white">
+            آنچه مشتریان ما می گویند
+          </h1>
+          <div className="flex justify-between gap-2">
+            <p className="dark:text-white_99 font-normal text-lg">
               داستان های موفقیت و توصیفات صمیمانه مشتریان گرامی را بخوانید. کشف
               کنید که چرا آنها Estatein را برای نیازهای املاک خود انتخاب کردند.
             </p>
-            <Link className="hidden sm:flex" to={"*"}>
+            <Link
+              className="hidden sm:flex p-3 border-[1px] rounded-lg  border-primary_green_light dark:text-white dark:bg-gray_10"
+              to={"/"}
+            >
               مشاهده بیشتر
             </Link>
           </div>
         </div>
-        <div className="w-96 h-96 rounded-lg border-2 border-primary_green_light">
-          <div>
-            <span className="w-6 h-6 rounded-full border-2 border-primary_green_light">
-              xx
-            </span>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
+        <div className="flex flex-col gap-8 w-96 h-96 rounded-lg p-9 border-[1px] border-primary_green_light">
+          <div className="flex gap-2">
+            <div className="flex justify-center items-center w-11 h-11 rounded-full border-[1px] border-gray-400 bg-[#262626]">
+              <img
+                src="./../src/assets/Star.svg"
+                alt="Star.svg"
+                className="w-6 h-6"
+              />
+            </div>
+            <div className="flex justify-center items-center w-11 h-11 rounded-full border-[1px] border-gray-400 bg-[#262626]">
+              <img
+                src="./../src/assets/Star.svg"
+                alt="Star.svg"
+                className="w-6 h-6"
+              />
+            </div>
+            <div className="flex justify-center items-center w-11 h-11 rounded-full border-[1px] border-gray-400 bg-[#262626]">
+              <img
+                src="./../src/assets/Star.svg"
+                alt="Star.svg"
+                className="w-6 h-6"
+              />
+            </div>
+            <div className="flex justify-center items-center w-11 h-11 rounded-full border-[1px] border-gray-400 bg-[#262626]">
+              <img
+                src="./../src/assets/Star.svg"
+                alt="Star.svg"
+                className="w-6 h-6"
+              />
+            </div>
+            <div className="flex justify-center items-center w-11 h-11 rounded-full border-[1px] border-gray-400 bg-[#262626]">
+              <img
+                src="./../src/assets/Star.svg"
+                alt="Star.svg"
+                className="w-6 h-6"
+              />
+            </div>
           </div>
-          <div>
-            <h3></h3>
-            <p></p>
+          <div className="flex flex-col gap-2">
+            <h3 className="font-medium text-2xl dark:text-white">
+              خدمات استثنایی!
+            </h3>
+            <p className="dark:text-white">
+              تجربه ما با Estatein فوق العاده بود. فداکاری و حرفه ای بودن تیم
+              آنها، یافتن خانه رویایی ما را آسان کرد. بسیار توصیه شده!
+            </p>
           </div>
-          <div></div>
+          <div className="flex gap-2">
+            
+            <img
+              src="./../src/assets/Profile1.png"
+              alt="Profile1.svg"
+              
+            />
+            <div className="flex flex-col gap-1">
+              <h3  className="text-xl font-normal">رضا فریدی</h3>
+              <p className="text-lg font-normal text-gray_60">مازندران،رشت</p>
+            </div>
+          </div>
         </div>
         <div></div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,22 @@ export default {
         },
         extend: {
             colors: {
-                primary_dark: "#37474F",
+                primary_dark: "#000000",
                 primary_green_light: "#8BC83F",
                 secondary_blue_light: "#1F4C6B",
+                gray_08: "#141414",
+                gray_10: "#1A1A1A",
+                gray_15: "#262626",
+                gray_20: "#333333",
+                gray_30: "#4D4D4D",
+                gray_40: "#666666",
+                gray_50: "#808080",
+                gray_60: "#999999",
+                white_90: "#E4E4E7",
+                white_95: "#F1F1F3",
+                white_96: "#F7F7F8",
+                white_99: "#FCFCFD",
+
             },
         },
     },
