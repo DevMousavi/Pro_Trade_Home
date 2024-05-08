2cac7f86cd6b0c92b0b24a97df063b9abcd7d6d9
Niayesh Kakekhani <Kakeniayesh@gmail.com>
2024-05-08 18:02:58 +0330

    Change font

--- src/Components/UserComments.jsx
+++ src/Components/UserComments.jsx
@@ -1,5 +1,6 @@
 // React ...
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 // Icons...
 
@@ -13,18 +14,25 @@ const UserComments = () => {
   }, []);
 
   return (
-    <div className="w-full flex justify-center dark:bg-primary_Color_Light">
-      <div className="w-96   xl:w-[1596px] lg:w-[1280px]  bg-gray-500">
-        <div>
-          <h1>آنچه مشتریان ما می گویند</h1>
-          <p>
-            داستان های موفقیت و توصیفات صمیمانه مشتریان گرامی را بخوانید. کشف
-            کنید که چرا آنها Estatein را برای نیازهای املاک خود انتخاب کردند.
-          </p>
+    <div className="w-full flex justify-center  dark:bg-primary_Color_Light">
+      <div className="flex flex-col gap-4 p-3 w-96 sm:w-[500px] md:w-[1000px] lg:w-[1280px]  xl:w-[1596px] bg-gray-500">
+        <div className="flex flex-col gap-2">
+          <h1 className="font-semibold text-2xl">آنچه مشتریان ما می گویند</h1>
+          <div className="flex justify-between ">
+            <p>
+              داستان های موفقیت و توصیفات صمیمانه مشتریان گرامی را بخوانید. کشف
+              کنید که چرا آنها Estatein را برای نیازهای املاک خود انتخاب کردند.
+            </p>
+            <Link className="hidden sm:flex" to={"*"}>
+              مشاهده بیشتر
+            </Link>
+          </div>
         </div>
-        <div>
+        <div className="w-96 h-96 rounded-lg border-2 border-primary_green_light">
           <div>
-            <span></span>
+            <span className="w-6 h-6 rounded-full border-2 border-primary_green_light">
+              xx
+            </span>
             <span></span>
             <span></span>
             <span></span>
