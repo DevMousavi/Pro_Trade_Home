ed373fa47eaf1237b524bb96a5b8f9edbfecb32b
DevMousavi <mousavii.dev@gmail.com>
2024-05-09 11:00:56 +0330

    The placement of buttons has been modified and the "About Us" box has been removed from the homepage

--- src/Components/Category.jsx
+++ src/Components/Category.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Category = () => {
     return (
-        <div className="my-10 w-full text-white as:mx-auto flex as:flex-col as:gap-4 xl:gap-16  md:flex-row md:w-[90%] md:mx-auto">
+        <div className="af:w-full flex flex-col gap-4">
             <Link
                 to=""
                 className="as:w-[90%] bg-white text-primary_dark font-bold dark:font-bold as:h-16 lg:h-20 flex items-center justify-center mx-auto rounded-xl boxShadowLight dark:text-white dark:bg-gray-950 cursor-pointer dark:boxShadowLight primaryTransition hover:text-xl  hover:text-pink-800"
